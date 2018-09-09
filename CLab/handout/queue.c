f8ed3e756f3bde51f57b9da1d105384891220c68
brucefeynman <brucefeynman1997@gmail.com>
2018-09-09 18:47:45 +0800

    finish lab0 with 100/100

--- CLab/handout/queue.c
+++ CLab/handout/queue.c
@@ -22,9 +22,14 @@
 */
 queue_t *q_new()
 {
-    queue_t *q =  malloc(sizeof(queue_t));
+    queue_t *q =  malloc(sizeof(queue_t)); 
+    // will return null if no sapce available
     /* What if malloc returned NULL? */
-    q->head = NULL;
+    if (q != NULL){
+      q->head = NULL;
+      q->tail = NULL;
+      q->size = 0;
+    }
     return q;
 }
 
@@ -32,8 +37,26 @@ queue_t *q_new()
 void q_free(queue_t *q)
 {
     /* How about freeing the list elements? */
-    /* Free queue structure */
-    free(q);
+    if (q != NULL){
+        list_ele_t *next_element;
+        for(int i=0;i<q->size;i++){
+            list_ele_t *element;
+            // get the element to free
+            if (i == 0){
+                element = q->head;
+            }else {
+                element = next_element;
+            }
+            // update for the next element
+            int last = q->size -1;
+            if (i != last){ // except the last one
+                next_element = element->next;
+            }
+            free(element);
+        }
+        /* Free queue structure */
+        free(q);
+    }
 }
 
 /*
@@ -43,13 +66,23 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, int v)
 {
-    list_ele_t *newh;
     /* What should you do if the q is NULL? */
+    if (q == NULL){
+        return false;
+    }
+    list_ele_t *newh;
     newh = malloc(sizeof(list_ele_t));
     /* What if malloc returned NULL? */
+    if (newh == NULL){
+        return false;
+    }
     newh->value = v;
     newh->next = q->head;
     q->head = newh;
+    q->size++;
+    if(q->tail == NULL){// check empty queue case
+        q->tail=newh;
+    }
     return true;
 }
 
@@ -63,7 +96,25 @@ bool q_insert_tail(queue_t *q, int v)
 {
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    if (q == NULL){
+        return false;
+    }
+    list_ele_t *newh;
+    newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL){
+        return false;
+    }
+    newh->value = v;
+    newh->next = NULL;
+    if (q->size != 0){// only update when there is an element
+        q->tail->next = newh;
+    }
+    q->tail = newh;
+    q->size++;
+    if(q->head == NULL){// check empty queue case
+        q->head=newh;
+    }
+    return true;
 }
 
 /*
@@ -76,7 +127,21 @@ bool q_insert_tail(queue_t *q, int v)
 bool q_remove_head(queue_t *q, int *vp)
 {
     /* You need to fix up this code. */
-    q->head = q->head->next;
+    if(q == NULL || q->size == 0){
+        return false;
+    }
+    list_ele_t *rm_element = q->head;
+    if (vp !=NULL){ // store value only when vp non-NULL
+        *vp = rm_element->value;
+    }
+    if (q->size == 1){// only one element left
+        q->head = NULL;
+        q->tail = NULL;
+    }else{
+        q->head = rm_element->next;
+    }
+    free(rm_element);
+    q->size--;
     return true;
 }
 
@@ -86,9 +151,12 @@ bool q_remove_head(queue_t *q, int *vp)
  */
 int q_size(queue_t *q)
 {
+    if (q == NULL){
+        return 0;
+    }
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    return q->size;
 }
 
 /*
@@ -101,5 +169,26 @@ int q_size(queue_t *q)
 void q_reverse(queue_t *q)
 {
     /* You need to write the code for this function */
+    if (q !=NULL && q->size != 0) {
+        q->tail = q->head;
+        list_ele_t *newq;
+        list_ele_t *curr_element;
+        list_ele_t *next_element;
+        // update the param
+        newq = q->head;
+        curr_element = q->head;
+        next_element = q->head->next;
+        // make last null
+        curr_element->next=NULL;
+        for (int i=1;i<q->size;i++){
+            newq=next_element;
+            if (i+1 != q->size){
+                next_element = next_element->next;
+            }
+            newq->next=curr_element;
+            curr_element=newq;
+        }
+        q->head=newq;
+    }
 }
 
